dbf8e3d19e817764945ec80e839872fa16f6f0d6
gil <m4ouyea@gmail.com>
2023-10-08 03:22:28 -0700

    wea

--- inicio.php
+++ inicio.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <script src="https://kit.fontawesome.com/ca2716a385.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <link rel="stylesheet" type="text/css" href="css/inicio.css">
     <meta charset="UTF-8">
     <title>Eclipse UX</title>
@@ -22,6 +24,17 @@
         </div>
     </section>
 
+    <section class="about-section">
+    <div class="about-content">
+        <h2>Acerca de Nosotros</h2>
+        <p>Somos un equipo apasionado de desarrolladores y entusiastas de la tecnología comprometidos en hacer que la colaboración en proyectos de código abierto y ciencia abierta sea más accesible y efectiva.</p>
+        <p>Nuestra aplicación tiene como objetivo conectar a creadores de proyectos con colaboradores interesados que posean las habilidades necesarias para contribuir. Facilitamos la comunicación y la creación conjunta de soluciones innovadoras para abordar desafíos en campos diversos.</p>
+        <div class="social-icons">
+            <a href="#" class="social-icon"><i class="fab fa-twitter" style="color: black; font-size: 30px;"></i></a>
+            <a href="#" class="social-icon"><i class="fab fa-instagram" style="color: black; black; font-size: 30px;"></i></a>
+            <a href="#" class="social-icon"><i class="fab fa-youtube" style="color: black; black; font-size: 30px;"></i></a>
+        </div>
+    </div></section>
 
     <section class="project-section">
         <h2>Proyectos Recientes</h2>
@@ -49,11 +62,15 @@
         <section class="wasa">
             <h2>Proyecto con mas interacciones</h2>
         </section>
-        <section class="wasa">
+        <section class="wasa2">
             <div class="wasatere">
-                <h3>Proyecto 1</h3>
-                <p>Descripción del proyecto 1.</p>
-            </div>
+                <div class="wasaimg">
+                <h3>Sistema de Gestión de Biblioteca Digital</h3>
+                <p>El proyecto de código abierto "Sistema de Gestión de Biblioteca Digital" tiene como objetivo proporcionar una solución completa y flexible para
+                     administrar bibliotecas digitales en línea. Este proyecto nace de la
+                     necesidad de facilitar el acceso a recursos digitales, libros electrónicos y otros materiales de lectura en formato digital.</p>
+                    </div>
+                </div>
         </section>
     </div>
 
